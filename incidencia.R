1d6762d383a59381e553d489395d1b8b9939abc2
isaias.morales <isaias.morales@conacyt.mx>
2023-02-17 10:51:16 -0600

    subir cambios de agregados

--- incidencia.R
+++ incidencia.R
@@ -1,31 +1,165 @@
 library(tidyverse)
 library(janitor)
 
-#
+setwd ('/home/isaias/analisis_datos/sesnsp_monitoreo/data')
 
 # Lectura de archivo comprimido
 
-df <- read_csv( unz( 'analisis_datos/sesnsp_monitoreo/data/data_original/Municipal-Delitos-2015-2022_jun2022.zip',
-              'Municipal-Delitos-2015-2022_jun2022/Municipal-Delitos-2015-2022_jun2022.csv'), 
-              locale = readr::locale(encoding = "Latin1"))
+file_zip <- unz( '/home/isaias/analisis_datos/sesnsp_monitoreo/data/data_original/Municipal-Delitos-2015-2022_dic2022.zip',
+                 'Municipal-Delitos-2015-2022_dic2022/Municipal-Delitos-2015-2022_dic2022.csv')
+file_zip
 
+# '~/Municipal-Delitos-2015-2022_jun2022.csv', 
+df <- read_csv( file_zip,
+              locale = readr::locale(encoding = "Latin1"))
 
 # Limpiar
 
 df <- clean_names(df)
-#names(df)
+names(df)
  
-
-# Crear primeros agregados 
-
-df1 <- df %>% rename( anio = ano, 
+df <- df %>% rename ( anio = ano, 
                    cve_mpio = cve_municipio) %>%
-  mutate( anio = as.character(anio),
+  mutate ( anio = as.character(anio),
+           cve_mpio = ifelse (str_length(cve_mpio)  <5, 
+                              paste0 ( '0' , cve_mpio ) , 
+                              cve_mpio),
           cve_mpio = as.factor(cve_mpio),
-           Clave_Ent = as.factor(clave_ent)) %>%
-  mutate(Total = rowSums(select_if(., is.numeric), na.rm = TRUE)) %>%
-  filter(tipo_de_delito == 'Feminicidio') %>%
-    group_by( anio, municipio,entidad  ) %>%
-    summarise( sum(Total)) 
-df1
+          clave_ent = ifelse (str_length(clave_ent)  == 1, 
+                  paste0 ( '0' , clave_ent ) , 
+                  clave_ent), 
+          clave_ent = as.factor(clave_ent) ) %>%  
+  mutate( total = rowSums(select_if(., is.numeric), na.rm = TRUE) )
 
+# df
+### Crear el catálogo de delitos  =====
+
+cat_del <- df %>% group_by(bien_juridico_afectado,
+                           tipo_de_delito,
+                           subtipo_de_delito,
+                           modalidad) %>% summarise()
+# view(cat_del)
+
+# df %>% filter(anio == 2022) %>% select(diciembre)
+
+### Nivel Nacional
+
+agrupado_nal <- function(df, delito, sub_delito) {
+  df <- df %>%
+    filter(tipo_de_delito == delito, 
+           subtipo_de_delito == sub_delito ) %>%
+    group_by( anio) %>%
+    summarise( total  =  sum(total))  %>%
+    
+    return(df)
+}
+
+df_nal <- agrupado_nal( df, 'Feminicidio', 'Feminicidio' )
+df_nal <- rename(df_nal, 'ci_femi' = 'total' )
+
+df_nal_hom <- agrupado_nal( df, 'Homicidio', 'Homicidio doloso')
+df_nal_hom <- rename(df_nal_hom, 'ci_hom_dol' = 'total' )
+df_nal_lxs <- agrupado_nal( df, 'Lesiones', 'Lesiones dolosas')
+df_nal_lxs <- rename(df_nal_lxs, 'ci_les_dol' = 'total' )
+
+df_nal <- merge(df_nal, df_nal_hom, by = 'anio')
+df_nal <- merge(df_nal, df_nal_lxs, by = 'anio')
+
+view(df_nal) 
+
+
+filename =  '/home/isaias/proyectos/vcmn/entropia_vcmn/datos/nacional/sesnsp_ci_nal.csv'
+write_csv(df_nal, filename)
+
+## Nivel estatal
+
+## Agregados, función
+
+agrupado <- function(df, delito, sub_delito) {
+  
+  df <- df %>%
+    filter(tipo_de_delito == delito, 
+           subtipo_de_delito == sub_delito ) 
+  
+  df <- df %>%
+    mutate( anio_lab = paste0('ci_', substr(delito, 1, 4) , '_', anio ) ) %>%
+    group_by( anio_lab, clave_ent, entidad) %>%
+    summarise( total_anio =  sum(total))  %>%
+    spread ( key = anio_lab , value = total_anio ) %>% clean_names()
+  return(df)
+}
+
+df_est <- agrupado( df, 'Feminicidio', 'Feminicidio' )
+df_hom <- agrupado( df, 'Homicidio', 'Homicidio doloso')
+df_lxs <- agrupado( df, 'Lesiones', 'Lesiones dolosas')
+
+df_est <- merge(df_est, df_hom, by = c('clave_ent', 'entidad'))
+df_est <- merge(df_est, df_lxs, by = c('clave_ent', 'entidad'))
+
+#view(df_est) 
+
+filename =  '/home/isaias/proyectos/vcmn/entropia_vcmn/datos/estatal/sesnsp_ci_estatal.csv'
+write_csv(df_est, filename)
+
+
+### Municipal ====
+
+agrupado <- function(df, delito, sub_delito) {
+df <- df %>%  filter( tipo_de_delito == delito,
+                      subtipo_de_delito == sub_delito ) %>% 
+  select( anio, 
+          #clave_ent, entidad, 
+          cve_mpio, municipio,
+          tipo_de_delito,
+          subtipo_de_delito,
+          total ) %>% 
+  mutate( anio_lab = paste0('ci_', delito , '_', anio ) ) %>%
+  group_by(anio_lab, 
+          # clave_ent, entidad,
+           cve_mpio, municipio,
+           tipo_de_delito) %>% 
+  summarise( total = sum(total))  %>%
+  spread( key = anio_lab, value = total) %>% 
+  select(- tipo_de_delito) %>% clean_names()
+
+return(df)
+}
+
+df_fem <- agrupado( df, 'Feminicidio','Feminicidio' )
+df_hom <- agrupado( df, 'Homicidio', 'Homicidio doloso')
+df_lxs <- agrupado( df, 'Lesiones', 'Lesiones dolosas')
+
+# Primero obtener todos los municipios
+
+mpios_sesnp <- df %>% group_by( clave_ent, entidad, cve_mpio, municipio) %>% summarise()
+#mpios_sesnp
+
+# Unir las tablas
+
+df_mpal <- merge(mpios_sesnp, df_fem, by = c('cve_mpio'), all.x = TRUE)
+df_mpal <- merge(df_mpal, df_hom, by = c('cve_mpio'), all.x = TRUE)
+df_mpal <- select(df_mpal, - c (municipio.x,municipio.y ) )
+df_mpal <- merge(df_mpal, df_lxs, by = c('cve_mpio'), all.x = TRUE)
+df_mpal <- df_mpal  %>% select(- c (municipio.x, municipio.y) ) %>% clean_names()
+df_mpal[is.na(df_mpal)] <- 0
+
+df_mpal
+
+filename =  '/home/isaias/proyectos/vcmn/entropia_vcmn/datos/municipal/sesnsp_ci_mpal.csv'
+write_csv(df_mpal, filename)
+
+###
+
+#### obtener municipios para evaluación
+
+mpios_sesnp <- df %>% group_by( clave_ent, entidad, cve_mpio, municipio) %>% summarise()
+
+mpios_sesnp
+
+# 2474 son menos 5 que están con clave no especificada
+
+filename =  '/home/isaias/analisis_datos/datos_mx_intergacion/datos/diagnostico_datos/sesnsp_mpios.csv'
+write_csv(mpios_sesnp, filename)
+
+# 2479
+#
--- victimas.R
+++ victimas.R
@@ -1,30 +1,88 @@
 library(tidyverse)
 library(readxl)
 library(janitor)
-
-#
-
 # Lectura de archivo comprimido
-
-#
 #unzip( "analisis_datos/sesnsp_monitoreo/data/data_original/Estatal-Víctimas-2015-2022_jun2022.zip", exdir="analisis_datos/sesnsp_monitoreo/data/data_original")
 
-df <- read_excel( 'analisis_datos/sesnsp_monitoreo/data/data_original/Estatal-Víctimas-2015-2022_jun2022.xlsx') 
+df <- read_excel( '/home/isaias/analisis_datos/sesnsp_monitoreo/data/data_original/Estatal-Víctimas-2015-2022_dic2022.xlsx') 
+#unique(df$Año)
 
 # Limpiar
 
 df <- clean_names(df)
 names(df)
- 
 
-# Crear primeros agregados 
+# limpieza y preparación
 
-df1 <- df %>% rename( anio = ano) %>%
+str(df)
+
+df <- df %>% rename( anio = ano) %>%
   mutate( anio = as.character(anio),
-          clave_ent = as.factor(clave_ent)) %>%
-  mutate(total = rowSums(select_if(., is.numeric), na.rm = TRUE)) %>%
-  filter(tipo_de_delito == 'Feminicidio') %>%
-    group_by( anio, entidad  ) %>%
-    summarise( sum(total)) 
-df1
+          clave_ent = ifelse (str_length(clave_ent)  == 1, 
+                              paste0 ( '0' , clave_ent ) , 
+                              clave_ent), 
+          clave_ent = as.factor(clave_ent) ) %>%
+  mutate(total = rowSums(select_if(., is.numeric), na.rm = TRUE)) 
+
+unique(df$anio)
+#head(df)
+#view(head(df, 15))
+#view(tail(df, 15))
+
+# Nacional
+
+
+agregado_nal <- function(df, delito, sub_del) {
+  suf <- substr(delito, 1, 3)                            
+  df <- df %>%
+    filter(tipo_de_delito == delito,
+           #sexo == 'Mujer',
+           subtipo_de_delito == sub_del ) %>%
+    group_by( anio, sexo) %>%
+    summarise( total = sum(total)) %>%
+    spread( key = sexo, value = total) %>%
+    rename_at(vars(-anio),function(x) paste0('v', suf, '_', x )) %>%
+    clean_names()
+  return(df)}
+
+df_nal <- agregado_nal( df, 'Feminicidio', 'Feminicidio' )
+df_nal_hom <- agregado_nal( df, 'Homicidio', 'Homicidio doloso')
+df_nal_lxs <- agregado_nal( df, 'Lesiones', 'Lesiones dolosas')
+
+df_nal <- merge(df_nal, df_nal_hom, by = 'anio')
+df_nal <- merge(df_nal, df_nal_lxs, by = 'anio')
+
+#view(df_nal) 
+
+filename =  '/home/isaias/proyectos/vcmn/entropia_vcmn/datos/nacional/sesnsp_vi_nal.csv'
+write_csv(df_nal, filename)
+
+## Estatal
+
+agregado_est <- function(df, delito, sub_del) {
+  suf <- substr(delito, 1, 3)                            
+  df <- df %>%
+    filter(tipo_de_delito == delito,
+           #sexo == 'Mujer',
+           subtipo_de_delito == sub_del ) %>%
+    mutate( anio_lab = paste0(suf, '_', anio ) ) %>%
+    group_by(clave_ent, entidad, anio_lab, sexo) %>% 
+    summarise( total = sum(total)) %>% mutate( col_lab = paste0('v_',  anio_lab, '_', sexo) ) %>% 
+    ungroup() %>%
+    select( -c(anio_lab, sexo)) %>% spread( key =  col_lab, value = total) %>% 
+    clean_names()
+  return(df)}
+
+df_est <- agregado_est( df, 'Feminicidio', 'Feminicidio' )
+df_hom <- agregado_est( df, 'Homicidio', 'Homicidio doloso')
+df_lxs <- agregado_est( df, 'Lesiones', 'Lesiones dolosas')
+
+df_est <- merge(df_est, df_hom, by = c('clave_ent', 'entidad'))
+df_est <- merge(df_est, df_lxs, by = c('clave_ent', 'entidad'))
+
+view(df_est) 
+
+filename =  '/home/isaias/proyectos/vcmn/entropia_vcmn/datos/estatal/sesnsp_vi_estatal.csv'
+write_csv(df_est, filename)
+
 
